bf2312fb4e0246e52ce31313e682684960bad57d
wangzhuoli <2150181185@qq.com>
2021-01-05 00:43:13 +0800

    img

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -13,7 +13,7 @@ const proSettings: DefaultSettings = {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: '管理系统',
   pwa: false,
   iconfontUrl: '',
   menu: {
---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const membert_status = { 0: '启用', '-1': '停用' }
